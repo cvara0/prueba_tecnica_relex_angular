0232a44d6d65e2179d5d951d948edf200cd20193
cvara0 <cvara.parra@gmail.com>
2023-12-15 22:18:03 -0300

    15-dic

--- src/app/services/post-list.service.ts
+++ src/app/services/post-list.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Post } from '../models/post.models';
 
 @Injectable({
   providedIn: 'root'
@@ -16,12 +17,30 @@ export class PostListService {
     return this.http.get(this.url);
   }
 
+  updatePost(post: Post){
+    fetch(`${this.url}/${post.id}`, {
+  method: 'PUT',
+  body: JSON.stringify({
+    id: post.id,
+    title: post.title,
+    body: post.body,
+    userId: post.userId,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+  }
 
   // Método para eliminar un elemento por su ID
   deletePost(postId: string) {
     fetch(`${this.url}/${postId}`, {
       method: 'DELETE',
-    }).then(resp=>alert("Elemento eliminado, codigo de respuesta: "+resp.status)).catch(resp=>alert("Error al eliminar elemento, codigo de respuesta: "+resp.status));
-
+    }).then(response=>response.status===200?alert("Elemento eliminado, codigo de respuesta: "+response.status):alert("Error al eliminar elemento, codigo de respuesta: "+response.status))
+      .catch(response=>alert("Error al eliminar elemento, codigo de respuesta: "+response.status));
   }
+
+  //seguir con sanitizer
 }
